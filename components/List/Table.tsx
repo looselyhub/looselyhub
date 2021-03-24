b6c476a9ddaa5fd2c2487d60817cdb4795ddc8e5
Fraps <fraps@msn.com>
2021-03-23 21:04:33 -0300

    React Table library added

---
+++ components/List/Table.tsx
@@ -0,0 +1,147 @@
+import React from 'react'
+import { useTable, usePagination } from 'react-table'
+
+export default function Table({ data }) {
+  const newRows = React.useMemo(() => createRows(), [data])
+  const newHeader = React.useMemo(() => createHeader(), [data])
+
+  function createHeader() {
+    if (data.length === 0) {
+      return []
+    }
+    const result = data[0].map((columnData) => {
+      return { Header: columnData.name, accessor: columnData.name }
+    })
+    return result
+  }
+
+  function createRows() {
+    const result = data.map((rowData) => {
+      const row = {}
+      rowData.forEach((columnData) => {
+        row[columnData.name] = columnData.value
+      })
+      return row
+    })
+    return result
+  }
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    { columns: newHeader, data: newRows, initialState: { pageIndex: 0 } },
+    usePagination
+  )
+
+  function renderHeader() {
+    return (
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()} key={'Header'}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()} key={column.id}>
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+    )
+  }
+
+  function renderBody() {
+    return (
+      <tbody {...getTableBodyProps()}>
+        {page.map((row) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()} key={`row+${row.id}`}>
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                    key={`row+${row.id}+${cell.column.id}`}
+                  >
+                    {cell.render('Cell')}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    )
+  }
+
+  function renderPagination() {
+    return (
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <table {...getTableProps()}>
+        {renderHeader()}
+        {renderBody()}
+      </table>
+      {renderPagination()}
+    </div>
+  )
+}
--- pages/list/[...id].tsx
+++ pages/list/[...id].tsx
@@ -1,12 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import DataEditor from '@glideapps/glide-data-grid'
-import {
-  DataEditorContainer,
-  GridColumn,
-  GridCell,
-} from '@glideapps/glide-data-grid'
 import { useRouter } from 'next/router'
+import Table from 'components/List/Table'
 
 export default function List() {
   const router = useRouter()
@@ -25,48 +20,14 @@ export default function List() {
     const path = router.asPath
     const pathParts = path.split('/')
     const id = pathParts[pathParts.length - 1]
-    if (id !== '[...id]') {
+    if (id !== '[...id]' && data.length === 0) {
       fetchData(id)
     }
   }, [router])
 
-  function getData([col, row]: readonly [number, number]): GridCell {
-    const dataRow = data[row]
-    const dataCol = dataRow[col]
-    return {
-      kind: dataCol.type,
-      data: dataCol.value,
-      displayData: dataCol.value,
-      allowOverlay: false,
-    }
-  }
-  function getColumns() {
-    // const firstRow = data[0];
-    const columns: GridColumn[] = data.map((column) => {
-      return {
-        title: column.title,
-        width: 100,
-      }
-    })
-    return columns
-  }
-
-  function renderGrid() {
-    return (
-      <DataEditorContainer width={500} height={300}>
-        <DataEditor
-          getCellContent={getData}
-          columns={getColumns()}
-          rows={data.length}
-        />
-      </DataEditorContainer>
-    )
-  }
-
   return (
     <div>
-      {renderGrid()}
-      <div id="portal" />
+      <Table data={data} />
     </div>
   )
 }
